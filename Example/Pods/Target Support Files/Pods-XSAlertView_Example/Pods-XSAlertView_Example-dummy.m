fb75b041665978106be82e27be19307ff2843682
邵晓飞 <shaoxiaof@hotmail.com>
2017-05-06 17:32:14 +0800

    no message

--- Example/Pods/Target Support Files/Pods-XSAlertView_Example/Pods-XSAlertView_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_XSAlertView_Example : NSObject
-@end
-@implementation PodsDummy_Pods_XSAlertView_Example
-@end
--- Example/Pods/Target Support Files/XSAlertView/XSAlertView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_XSAlertView : NSObject
-@end
-@implementation PodsDummy_XSAlertView
-@end
